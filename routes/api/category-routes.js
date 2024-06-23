dfe273ccd465233fc0076e0caac8db8c3cb76fb0
rozierhj <160989130+rozierhj@users.noreply.github.com>
2024-06-23 11:14:07 -0400

    added search for categories all or one category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,38 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try{
+
+    const allCategories = await Category.findAll({
+      include:[{model: Product}],
+    });
+    res.status(200).json(allCategories);
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try{
+
+    const oneCategory = await Category.findByPk(req.params.id,{
+      include:[{model:Product}],
+    });
+    if(!oneCategory){
+      res.status(400).json({message: "No category found with that id"});
+      return;
+    }
+    res.status(200).json(oneCategory);
+
+  }catch(err){
+    res.status(500).json(err);
+  }
+
 });
 
 router.post('/', (req, res) => {
